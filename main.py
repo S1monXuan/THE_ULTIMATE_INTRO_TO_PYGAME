0e544c2d5ba3903ddb778afb408106edf236e8e0
SimonXuan <xxmspk@163.com>
2024-12-09 21:16:34 -0500

    set rect for both snail and player

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ pygame.display.set_caption('Runner') # Set name of pygame
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My Game', False, 'black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (900, 300))
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -19,6 +25,13 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    snail_rect.left -= 4
+    if snail_rect.right < 0:
+        snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    player_rect.left += 1
+    screen.blit(player_surf, player_rect)
+    
 
     # draw all elements & update everyting
     pygame.display.update()
