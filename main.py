7220ded59d88adc5cc947f0485dfb9aa745753e2
SimonXuan <xxmspk@163.com>
2024-12-10 00:46:28 -0500

    color settings

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My Game', False, 'black')
+# text_surface = test_font.render('My Game', False, 'black')
+score_surf = test_font.render('Score', False, (64, 64, 64))
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (900, 300))
@@ -21,10 +23,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            # print(event.pos)
+            if player_rect.collidepoint(event.pos):
+                print("collision")
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    # screen.blit(text_surface, (300, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+
+
+    pygame.draw.line(screen, 'Blue', (0, 0), (800, 400))
+    pygame.draw.line(screen, 'Blue', (0, 0), (800, 400), 5)
+
+    # pygame.draw.line(screen, 'Blue', (0, 0), pygame.mouse.get_pos())
+    # pygame.draw.line(screen, 'Blue', (0, 0), pygame.mouse.get_pos(), 5)
+    
+    screen.blit(score_surf, score_rect)
 
     snail_rect.left -= 4
     if snail_rect.right < 0:
@@ -34,8 +51,13 @@ while True:
     player_rect.left += 1
     screen.blit(player_surf, player_rect)
     
-    if player_rect.colliderect(snail_rect):
-        snail_rect.left = 800
+    # if player_rect.colliderect(snail_rect):
+    #     snail_rect.left = 800
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print('Collide')
+
 
     # draw all elements & update everyting
     pygame.display.update()
